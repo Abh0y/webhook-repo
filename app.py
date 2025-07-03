06d1da96d4df5505034f05549b1c9089ddd08bea
delusional4Everything <Neothematrix1811@gmail.com>
2025-07-03 14:20:58 +0530

    deployment setup

--- app.py
+++ app.py
@@ -64,4 +64,6 @@ def get_events():
     return jsonify(results)
 
 if __name__ == '__main__':
-    app.run(port=5000)
+   port = int(os.environ.get("PORT", 5000))
+   app.run(host="0.0.0.0", port=port)
+
